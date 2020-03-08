9d443b40c1a3500836c1c819a4efe8e1f5154800
ahmedti <ARBYL-EI.NT_Project@arcelik.com>
2020-03-09 00:14:49 +0300

    add more section

---
+++ functions_importants.py
@@ -0,0 +1,16 @@
+# some important functions/methods for all area..
+import sys
+
+# is the object element of that class question? isinstance(x,y)
+class Engine:
+    pass
+
+class Truck(Engine):
+    def name():
+        pass
+
+eng = Engine()
+truck = Truck()
+
+print(isinstance(eng, Engine))
+print(isinstance(eng,Truck)) #as youu see the answers is obvious.. .
--- maths.py
+++ maths.py
@@ -8,3 +8,19 @@ try:
     print(min(mixList), max(mixList))
 except Exception as e:
     print("does not support compare int - str etc... ")
+
+# also have round(x), abs(x), pow(x,y) we have to use in standard library..
+
+# sorted(x) use almost for everythngs....
+list_sort = [1,2,3,4,5,'ali', 'mom', 0x44]
+try:
+    print(sorted(list_sort))
+except Exception as e:
+    print("does not support sorted int - str etc... ")
+
+list_sort = ['ali', 'veli', 'M', '1']
+print(sorted(list_sort))
+
+#use math module for all mathemaical functions... pretty useful
+import math 
+print(math.sin(math.pi/2))
