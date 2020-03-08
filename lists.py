16c13b9b8a1f610c485699d332a0c5d8e154a9db
ahmedti <ARBYL-EI.NT_Project@arcelik.com>
2020-03-08 15:11:07 +0300

    add more section

---
+++ lists.py
@@ -0,0 +1,13 @@
+### LIST ###
+list1 = ['ali', 'cal', 1,3, 's', [23,"1"]]
+print (list1)
+
+list1.append('veli') #only add single element...
+print(list1)
+
+list1.extend(['kim' , 1]) #add with matrix
+print(list1)
+
+#lets write in a loop
+for l in list1:
+    print(l)
---
+++ strings.py
@@ -0,0 +1,6 @@
+
+phrase = "we are going today ... "
+
+#calculate the length of string...
+print(phrase.__len__())
+print(len(phrase))
