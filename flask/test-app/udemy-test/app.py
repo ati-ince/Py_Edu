ee2b1b0d4653c20be1f4cbc41a781206625b1f7f
ati-ince <atahir.ince@gmail.com>
2020-08-06 06:03:11 +0000

    add html form and modif flask jypiter

--- flask/test-app/udemy-test/app.py
+++ flask/test-app/udemy-test/app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route('/form')
+def form():
+    return render_template("form.html")
+
 if __name__ == '__main__':
     app.run()
